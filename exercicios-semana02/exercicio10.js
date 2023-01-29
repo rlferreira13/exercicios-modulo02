5f0d2f38f8387bf01acd438efc7eb5119249aff8
rlferreira13 <rlferreira13@gmail.com>
2023-01-29 20:40:54 -0300

    Finalizado questão numero 10

--- exercicios-semana02/exercicio10.js
+++ exercicios-semana02/exercicio10.js
@@ -1,15 +1,29 @@
 /*Pensando em um sistema de notas online, crie um código que irá capturar o nome do aluno e a nota de 4 unidades usando o prompt dentro de uma laço while, a ideia é que a cada valor digitado seja puxado para um array com o método push. Após isso, crie um laço “for” que irá calcular a soma de todos os itens do array para ser calculada sua média.
 Ao final de tudo, deverá ser apresentado em tela o nome do aluno, sua média e se o aluno foi aprovado ou não, para esse último tópico considere a média para aprovação maior ou igual a 7.*/
 let nomeAluno;
-let array =[];
+let arrayGeral = [];
+let arrNotas = [];
 let i = 1;
+let somadasNotas = 0;
+
 while (i <= 4) {
-    if (!nomeAluno ){
-        nomeAluno = window.prompt("Informe o nome do aluno:");
-        array.push (nomeAluno);
-    }
-    let nota = window.prompt(`Informe a nota do ${i}° bimestre: `);
-    array.push (nota);
-    i++;
+  if (!nomeAluno) {
+    nomeAluno = window.prompt("Informe o nome do aluno:");
+    arrayGeral.push(nomeAluno);
+  }
+  let nota = parseInt(window.prompt(`Informe a nota do ${i}° bimestre: `));
+  arrayGeral.push(nota);
+  i++;
 }
-console.log(array)
+for (const notas of arrayGeral) {
+  if (Number.isInteger(notas)) {
+    arrNotas.push(notas);
+  }
+}
+
+for (let j = 0; j < arrNotas.length; j++) {
+  somadasNotas += arrNotas[j];
+}
+
+document.write(`O aluno ${nomeAluno} teve a nota final: ${somadasNotas / 4}.<br>`);
+let passouDeAno = somadasNotas / 4 < 7 ? document.write("Ele foi reprovado.") : document.write("Ele foi aprovado");
